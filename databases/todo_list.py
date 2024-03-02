48b37d4fe2aff30c267df8ae5f1ec8fbed8694b7
nexblu <farras.pramudita@gmail.com>
2024-03-02 23:23:39 +0700

    refactor(api todo): refactor crud & database

--- databases/todo_list.py
+++ databases/todo_list.py
@@ -1,9 +1,22 @@
 from .config import db_session, init_db
 from models import TodoList
+from .database import Database
 
 
-def add_todo_list(username, task):
-    init_db()
-    todo_list = TodoList(username, task)
-    db_session.add(todo_list)
-    db_session.commit()
+class TodolistDatabase:
+    def __init__(self) -> None:
+        init_db()
+
+    async def insert(self, username, task):
+        todo_list = TodoList(username, task)
+        db_session.add(todo_list)
+        db_session.commit()
+
+    async def delete(self):
+        pass
+
+    async def get(self):
+        pass
+
+    async def update(self):
+        pass
--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from flask_cors import CORS
 from config import debug_mode
 from routers.login import login_router
 from routers.user import user_router
+from routers.todo_list import todo_list_router
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
 app.register_blueprint(login_router)
 app.register_blueprint(user_router)
+app.register_blueprint(todo_list_router)
 
 
 if __name__ == "__main__":
