f628ea10db5286184e99298367e3696bfef28952
nexblu <farras.pramudita@gmail.com>
2024-03-10 12:37:24 +0700

    fix(todo list): update structure api add task

--- databases/todo_list.py
+++ databases/todo_list.py
@@ -9,8 +9,8 @@ class TodolistDatabase(Database):
         super().__init__()
         init_db()
 
-    async def insert(self, username, task, created_at, is_done):
-        todo_list = TodoList(username, task, created_at, is_done)
+    async def insert(self, username, task, created_at):
+        todo_list = TodoList(username, task, created_at)
         db_session.add(todo_list)
         db_session.commit()
 
