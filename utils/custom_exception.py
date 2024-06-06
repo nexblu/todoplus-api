92e00a36c80f3b7a2202fa4f096cf80aef400673
nexblu <farras.pramudita@gmail.com>
2024-06-06 13:41:25 +0700

    feat(exception): add password not secure exception

--- utils/custom_exception.py
+++ utils/custom_exception.py
@@ -2,3 +2,9 @@ class EmailNotValid(Exception):
     def __init__(self, message="email not active"):
         self.message = message
         super().__init__(self.message)
+
+
+class PasswordNotSecure(Exception):
+    def __init__(self, message="password not secure"):
+        self.message = message
+        super().__init__(self.message)
