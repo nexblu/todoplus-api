6f3687417fb3334e24d2bb0d9e8445bc6017e755
nexblu <farras.pramudita@gmail.com>
2024-06-06 12:10:25 +0700

    feat(database): add database mongodb to limiter

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from config import debug_mode
-from routers.login import login_router
-from routers.user import user_router
-from routers.todo_list import todo_list_router
-from routers.register import register_router
-from routers.reset_password import reset_router
-from routers.account_active import account_active_router
-from routers.email import email_router
+from config import debug_mode, mongodb_url
 from databases import db_session
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from utils import handle_404, handle_415, handle_429, handle_400
-from flask_github import GitHub
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
 limiter = Limiter(
-    get_remote_address, app=app, default_limits=[""], storage_uri="memory://"
+    get_remote_address, app=app, default_limits=[""], storage_uri=mongodb_url
 )
 
 
@@ -37,19 +29,6 @@ async def shutdown_session(exception=None):
 @app.teardown_request
 async def checkin_db(exc):
     db_session.remove()
-
-
-app.register_blueprint(login_router)
-app.register_blueprint(user_router)
-app.register_blueprint(todo_list_router)
-app.register_blueprint(register_router)
-app.register_blueprint(reset_router)
-app.register_blueprint(account_active_router)
-app.register_blueprint(email_router)
-app.register_error_handler(429, handle_429)
-app.register_error_handler(404, handle_404)
-app.register_error_handler(415, handle_415)
-app.register_error_handler(400, handle_400)
 
 
 if __name__ == "__main__":
