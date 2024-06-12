d2c5efe86fa875c2e6cc16a35a8061e84816bbb2
nexblu <farras.pramudita@gmail.com>
2024-06-12 20:32:27 +0700

    refactor: make api todo list as done as modular

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ from api.login import login_router
 from api.todo_list import todo_list_router
 from api.account_active import account_active_router
 from api.refresh_token import refresh_token_router
+from api.is_done import todo_list_is_done_router
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -49,6 +50,7 @@ app.register_blueprint(login_router)
 app.register_blueprint(todo_list_router)
 app.register_blueprint(account_active_router)
 app.register_blueprint(refresh_token_router)
+app.register_blueprint(todo_list_is_done_router)
 
 app.register_error_handler(429, handle_429)
 app.register_error_handler(404, handle_404)
