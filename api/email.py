36c4387905f403b07fce75957dccf11eae8d4b94
nexblu <farras.pramudita@gmail.com>
2024-06-13 09:39:25 +0700

    refactor: make api email validator as modular

--- api/email.py
+++ api/email.py
@@ -1,31 +1,9 @@
-from flask import Blueprint, jsonify
-from email_validator import validate_email, EmailNotValidError
+from flask import Blueprint
+from controllers import EmailController
 
 email_router = Blueprint("api email", __name__)
 
 
 @email_router.get("/todoplus/v1/email-validator/<string:email>")
 async def email_validator(email):
-    try:
-        emailinfo = validate_email(email, check_deliverability=False)
-        email = emailinfo.normalized
-    except EmailNotValidError as e:
-        return (
-            jsonify(
-                {
-                    "status_code": 404,
-                    "message": f"email not valid {email!r}",
-                }
-            ),
-            404,
-        )
-    else:
-        return (
-            jsonify(
-                {
-                    "status_code": 200,
-                    "message": f"email is valid {email!r}",
-                }
-            ),
-            200,
-        )
+    return await EmailController.email_validator(email)
--- controllers/__init__.py
+++ controllers/__init__.py
@@ -2,3 +2,4 @@ from .login_controller import *
 from .todo_list_controller import *
 from .bookmark_controller import *
 from .account_active_controller import *
+from .email_controller import *
