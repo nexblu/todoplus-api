ecfcdda0dc6b25d4601eed062dad8b8db58966f3
nexblu <farras.pramudita@gmail.com>
2024-06-16 12:25:42 +0700

    refactor(bookmark): refactor code insert bookmark pin by id

--- database/bookmark_pin.py
+++ database/bookmark_pin.py
@@ -2,8 +2,7 @@ from .config import db_session, init_db
 from models import BookmarkPinDatabase, TodoListDatabase
 from .database import Database
 from sqlalchemy import and_, desc
-from utils import UserNotFound, TaskNotFound, BookmarkAlreadyPinned
-import datetime
+from utils import TaskNotFound, BookmarkAlreadyPinned
 from .todo_list import TodoListCRUD
 
 
@@ -14,34 +13,40 @@ class BookmarkPinCRUD(Database):
         self.todo_list_database = TodoListCRUD()
 
     async def insert(self, user, task_id, bookmark_id, created_at):
-        if (
-            data := TodoListDatabase.query.filter(
-                and_(
-                    TodoListDatabase.id == task_id,
-                    TodoListDatabase.user_id == user.id,
-                )
-            )
-            .order_by(desc(TodoListDatabase.created_at))
-            .first()
-        ):
-            if not (
-                data := BookmarkPinDatabase.query.filter(
+        if not (
+            task := (
+                TodoListDatabase.query.filter(
                     and_(
-                        BookmarkPinDatabase.task_id == task_id,
-                        BookmarkPinDatabase.user_id == user.id,
+                        TodoListDatabase.id == task_id,
+                        TodoListDatabase.user_id == user.id,
                     )
                 )
-                .order_by(desc(BookmarkPinDatabase.created_at))
+                .order_by(desc(TodoListDatabase.created_at))
                 .first()
-            ):
-                bookmark_pin = BookmarkPinDatabase(
-                    user.id, task_id, bookmark_id, created_at
+            )
+        ):
+            raise TaskNotFound
+        if bookmark_pin := (
+            BookmarkPinDatabase.query.filter(
+                and_(
+                    BookmarkPinDatabase.task_id == task_id,
+                    BookmarkPinDatabase.user_id == user.id,
                 )
-                db_session.add(bookmark_pin)
-                db_session.commit()
-                return bookmark_pin
+            )
+            .order_by(desc(BookmarkPinDatabase.created_at))
+            .first()
+        ):
             raise BookmarkAlreadyPinned
-        raise TaskNotFound
+        new_bookmark_pin = BookmarkPinDatabase(
+            user_id=user.id,
+            task_id=task_id,
+            bookmark_id=bookmark_id,
+            created_at=created_at,
+        )
+        db_session.add(new_bookmark_pin)
+        db_session.commit()
+
+        return new_bookmark_pin
 
     async def delete(self, category, **kwargs):
         pass
